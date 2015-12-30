0ef3646f081dd8cd21883820a58e06ebd545e0f3
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-12-30 14:35:11 -0800

    Initial Commit

---
+++ coursera_algorithm_week4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coursera_algorithm_week4
+//
+//  Created by 安田洋介 on 12/30/15.
+//  Copyright © 2015 安田洋介. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
